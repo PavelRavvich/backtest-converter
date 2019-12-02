6220adc738ec414ea1224d96e0fbe48e0f5899b4
Pavel Ravvich <triodjangopiter@yandex.ru>
2019-12-02 08:55:08 +0300

    ref (param filter) service filtering

--- src/app/modules/converter/helpers/backtest/backtest.helper.ts
+++ src/app/modules/converter/helpers/backtest/backtest.helper.ts
@@ -1,37 +1,27 @@
-import { KeyValue } from '@angular/common';
-
 // Interfaces
 import { IBacktest } from '../../interfaces';
 
 export function parseBacktest(data: string): IBacktest {
     const item = data.split('\t');
-    const backtest = {
+    return  {
         number: +item[0],
         profit: +item[1],
         value: +item[2],
         profitable: +item[3],
         mathExpectation: +item[4],
         dropDownCurrency: +item[5],
-        dropDownPercent: +item[6].slice(0, -1),
         profitToDropDown: +item[1] / +item[5],
+        dropDownPercent: +item[6].slice(0, -1),
+        ... parseParams(item.slice(8, item.length)),
     };
-
-    parseParams(item.slice(8, item.length))
-        .forEach(param => backtest[param.key] = param.value);
-
-    return backtest;
 }
 
-export function parseParams(data: string[]): Array<KeyValue<string, number>> {
-    const params = new Array<KeyValue<string, number>>();
+export function parseParams(data: string[]): any {
+    const params = {} as any;
     data.forEach(item => {
-        const arr = item
-            .trim()
+        const arr = item.trim()
             .split('=');
-        params.push({
-            key: arr[0],
-            value: +arr[1]
-        });
+        params[arr[0]] = +arr[1];
     });
     return params;
 }
